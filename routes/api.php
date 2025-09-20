423d9594f21d10a5afc4892f33e502c5c15ea7fd
ma7moud3othman <mahmoud.othman.mostafa@gmail.com>
2025-09-20 10:42:01 +0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test-api', function () {
     return response()->json(['message' => 'API is working fine']);
 });
+Route::get('/zep', function () {
+    return response()->json(['message' => 'API is working fine']);
+});
