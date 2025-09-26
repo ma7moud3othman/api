71330a2ffb2543c2956898472e636e4fe617a58a
ma7moud3othman <mahmoud.othman.mostafa@gmail.com>
2025-09-26 16:43:51 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 
 
-Route::get('/test-api', function () {
-
+Route::get('/show', function () { 
 $posts =  Post::all();
+    return response()->json(['posts' => $posts], 200);
+});
+Route::get('/create', function () { 
+  $posts = Post::factory()->count(10)->create();
     return response()->json(['posts' => $posts], 200);
 });
